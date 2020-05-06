c7bccb692cd74344e6aa20faf8266ab7b6942a63
Md Sajjad Ansari <64423450+mdsajjadansari@users.noreply.github.com>
2020-05-06 23:16:44 +0530

    Configuration File

---
+++ conf.py
@@ -0,0 +1,8 @@
+SSID = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+AUTH_TOKEN = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+FROM_NUMBER = '+12xxxxxxxxxx'
+TO_NUMBER = '+9172xxxxxxxx'
+API_KEY = '0c6xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+DEVICE_ID = 'BOLTxxxxxxxxxx'
+FRAME_SIZE = 10
+MUL_FACTOR = 6
